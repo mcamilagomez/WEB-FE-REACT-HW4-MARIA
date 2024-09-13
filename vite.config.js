5e28f3f47924c4b62e848681f2041d1e5898dafd
mcamilagomez <blanquicettm@uninorte.edu.co>
2024-09-13 10:33:57 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "https://mcamilagomez.github.io/WEB-FE-REACT-HW4-MARIA"
+})
